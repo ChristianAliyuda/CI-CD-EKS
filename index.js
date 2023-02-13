6483e2e0b9bc6337c0570966f5d4b76ca3af355b
kamran khan <91130565+kamrankamrankhan@users.noreply.github.com>
2023-02-14 01:51:29 +0500

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const config = require('config')
 console.log(config);
 
 app.get('/', (req, res) => {
-  res.send('Hello world, this is a demo for eks deployment using ')
+  res.send('Hello world,')
 })
 
 app.get('/status', (req, res) => {
