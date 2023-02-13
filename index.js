4ab1c03b1f91503a9988ba0a25771fc60860eeb5
kamran khan <91130565+kamrankamrankhan@users.noreply.github.com>
2023-02-14 03:07:06 +0500

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const config = require('config')
 console.log(config);
 
 app.get('/', (req, res) => {
-  res.send('Hello world,')
+  res.send('Hello world hope youdoing well,')
 })
 
 app.get('/status', (req, res) => {
